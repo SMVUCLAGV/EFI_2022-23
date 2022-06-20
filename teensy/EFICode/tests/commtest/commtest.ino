4aefac394e33d7d15a8a956c89aea11e86a13073
mikequ1 <mikequmrsh@gmail.com>
2022-06-19 21:27:37 -0700

    successful single byte UART test

--- teensy/EFICode/tests/commtest/commtest.ino
+++ teensy/EFICode/tests/commtest/commtest.ino
@@ -1,9 +1,22 @@
 void setup() {
-  // put your setup code here, to run once:
-
+    Serial.begin(9600);
+    Serial.println("Initializing...");
+    Serial1.begin(9600); // According to the Teensy Docs, this is the RX1 & TX1 on my board.
+    // Serial itself corrosponds to the micro-usb port
 }
+String msg = "";      
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    if(Serial1.available() > 0) {
+      //Serial.println("Serial 1 available");
+      msg = "";
+      while(Serial1.available() > 0) {
+          char rd = Serial1.read();
+          msg += rd;
+      }
+      Serial1.write('X'); // Acknowledge with reply
+      Serial.println(msg); // Output to console for debugging
+    }
 
+    // Should be a number 1-9
 }
